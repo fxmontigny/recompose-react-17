dad38549d2782166af939468c53b3e5d236fd8eb
François-Xavier Montigny <fxaviermontigny@gmail.com>
2022-06-13 12:47:46 +0200

    publish build

--- src/packages/recompose-react-17/__tests__/setDisplayName-test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { setDisplayName } from '../'
-/* eslint-disable */
-
-test('setDisplayName sets a static property on the base component', () => {
-  const BaseComponent = () => <div />
-  const NewComponent = setDisplayName('Foo')(BaseComponent)
-  expect(NewComponent.displayName).toBe('Foo')
-})
